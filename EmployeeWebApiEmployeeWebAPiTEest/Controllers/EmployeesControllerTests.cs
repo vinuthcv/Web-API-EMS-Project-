1d346f275c767e7455ff08c9c87d766bdc903686
vinuthcv <cvvinuth@gmail.com>
2019-07-27 19:52:03 +0530

    unit test updated for business layer

--- EmployeeWebApiEmployeeWebAPiTEest/Controllers/EmployeesControllerTests.cs
+++ EmployeeWebApiEmployeeWebAPiTEest/Controllers/EmployeesControllerTests.cs
@@ -16,7 +16,7 @@ namespace EmployeeWebApi.Controllers.Tests
     [TestClass()]
     public class EmployeesControllerTests
     {
-        int employeeId = 17;
+       
         [TestMethod()]
         public void AddEmployeeTest()
         {
@@ -65,11 +65,20 @@ namespace EmployeeWebApi.Controllers.Tests
                 Username = null
             };
             var mockEmployeeManager = new Mock<IEmployeeManager>();
-            mockEmployeeManager.Setup(x => x.AddEmployee(employee)).Returns(2);
+            mockEmployeeManager
+                .Setup(x => x.AddEmployee(employee))
+                .Returns(2)
+                .Callback(() =>
+                {
+                    throw new Exception("Username cannot be null");
+                });
 
+            // act calling method with test/mock data.
             var controller = new EmployeesController(mockEmployeeManager.Object);
             IHttpActionResult result = controller.Post(employee);
+            var negResult = result as NegotiatedContentResult<string>;
 
+            //assert: verifying the result
             Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result, typeof(ExceptionResult));
         }
@@ -77,26 +86,71 @@ namespace EmployeeWebApi.Controllers.Tests
         [TestMethod()]
         public void GetEmployeesTest()
         {
+            //arrange: the mock data creation.
+            var employees = new List<EmployeeEntities.Employee>
+            {
+                new EmployeeEntities.Employee{
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "VinuthTest1",
+                Id = 1
+                
+                },
+                new EmployeeEntities.Employee{
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "VinuthTest2",
+                Id = 2
+                }
+            };
             var mockEmployeeManager = new Mock<IEmployeeManager>();
+            mockEmployeeManager
+                .Setup(x => x.GetEmployees())
+                .Returns(employees);
+
+            //act: calling method with mocked object
             var controller = new EmployeesController(mockEmployeeManager.Object);
             var result = controller.Get();
-            Assert.IsNotNull(result);
-
             var negResult = result as OkNegotiatedContentResult<IEnumerable<EmployeeEntities.Employee>>;
 
+            //assert: verifying the result
+            Assert.IsNotNull(negResult);
             Assert.IsTrue(negResult.Content.Any());
         }
 
         [TestMethod()]
         public void GetEmployeeByIdTest()
         {
+            int employeeId = 2;
+            //arrange: mock test data
+            var employee = new EmployeeEntities.Employee
+            {
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "vinuth" + DateTime.Now.Millisecond + "",
+                Id=2
+            };
             var mockEmployeeManager = new Mock<IEmployeeManager>();
+            mockEmployeeManager
+                .Setup(x => x.GetEmployee(employeeId))
+                .Returns(employee);
+                
+            // act: test mocked data.
             var controller = new EmployeesController(mockEmployeeManager.Object);
             var result = controller.Get(employeeId);
-            Assert.IsNotNull(result);
-
             var negResult = result as OkNegotiatedContentResult<EmployeeEntities.Employee>;
 
+            //assert: verify the results.
+            Assert.IsNotNull(negResult);
             Assert.IsTrue(negResult.Content.Id == employeeId);
         }
 
@@ -104,43 +158,86 @@ namespace EmployeeWebApi.Controllers.Tests
         [TestMethod()]
         public void GetEmployeeByIdNotFoundTest()
         {
+            int employeeId = 0;
+            //arrange: mock test data
+            var employee = new EmployeeEntities.Employee
+            {
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "vinuth" + DateTime.Now.Millisecond + "",
+                Id = 2
+            };
             var mockEmployeeManager = new Mock<IEmployeeManager>();
-            var controller = new EmployeesController(mockEmployeeManager.Object);
-            var result = controller.Get(0);
-            Assert.IsNotNull(result);
+            mockEmployeeManager
+                .Setup(x => x.GetEmployee(0))
+                .Returns(employee);
 
-            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+            // act: test mocked data.
+            var controller = new EmployeesController(mockEmployeeManager.Object);
+            var result = controller.Get(employeeId);
+            var negResult = result as NotFoundResult;
+
+            //assert: verify the results.
+            Assert.IsNotNull(negResult);
         }
 
         [TestMethod()]
         public void DeleteEmployeeByIdNotFoundTest()
         {
+            //arrange: mocking the data.
+
             var mockEmployeeManager = new Mock<IEmployeeManager>();
+
+            //act: test mocked data.
             var controller = new EmployeesController(mockEmployeeManager.Object);
             var result = controller.Delete(0);
-            Assert.IsNotNull(result);
             var negResult = result as StatusCodeResult;
+
+            //assert: verify the results.
+            Assert.IsNotNull(result);            
             Assert.AreEqual(negResult.StatusCode,HttpStatusCode.NotFound);
         }
 
         [TestMethod()]
         public void DeleteEmployeeByIdTest()
         {
+            //arrange: mock the data.
+            var employeeId = 2;
             var mockEmployeeManager = new Mock<IEmployeeManager>();
+            mockEmployeeManager
+                .Setup(x => x.DeleteEmployee(employeeId))
+                .Returns(true);
+                
+            //act: call the method with mocked data.
             var controller = new EmployeesController(mockEmployeeManager.Object);
             var result = controller.Delete(employeeId);
-            Assert.IsNotNull(result);
+            var negResult = result as StatusCodeResult;
+
+            //assert: verify the results.
+            Assert.IsNotNull(negResult);
+            Assert.AreEqual(negResult.StatusCode, HttpStatusCode.NoContent);
+
         }
 
         [TestMethod()]
         public void CheckLoginSuccessMethod()
         {
+            //arrange
             var mockEmployeeManager = new Mock<IEmployeeManager>();
-            var controller = new EmployeesController(mockEmployeeManager.Object);
-            var result = controller.CheckLogin(new Models.Login { Password = "test123", UserName="vinuth15"});
-            Assert.IsNotNull(result);
+            mockEmployeeManager
+                .Setup(x => x.CheckLogin("vinuth", "test123"))
+                .Returns(true);
 
-            var negResult = result as StatusCodeResult;
+            //act 
+            var controller = new EmployeesController(mockEmployeeManager.Object);
+            var result = controller.CheckLogin(new Models.Login { Password = "test123", UserName="vinuth"});
+            var negResult = result as NegotiatedContentResult<string>;
+
+            //assert
+            Assert.IsNotNull(negResult);
             Assert.AreEqual(negResult.StatusCode,HttpStatusCode.OK);
         }
 
@@ -148,12 +245,19 @@ namespace EmployeeWebApi.Controllers.Tests
         [TestMethod()]
         public void CheckLoginFailMethod()
         {
+            // arrange
             var mockEmployeeManager = new Mock<IEmployeeManager>();
-            var controller = new EmployeesController(mockEmployeeManager.Object);
-            var result = controller.CheckLogin(new Models.Login { Password = "test1234", UserName = "vinuth15" });
-            Assert.IsNotNull(result);
+            mockEmployeeManager
+                .Setup(x => x.CheckLogin("vinuth", "test123"))
+                .Returns(false);
 
-            var negResult = result as StatusCodeResult;
+            //act 
+            var controller = new EmployeesController(mockEmployeeManager.Object);
+            var result = controller.CheckLogin(new Models.Login { Password = "test123", UserName = "vinuth" });
+            var negResult = result as NegotiatedContentResult<string>;
+
+            //assert
+            Assert.IsNotNull(negResult);
             Assert.AreEqual(negResult.StatusCode, HttpStatusCode.Forbidden);
         }
     }
---
+++ EmployeeWebApiEmployeeWebAPiTEest/EmployeeManagerTests.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using EmployeeData;
+
+namespace EmployeeCore.Tests
+{
+    [TestClass()]
+    public class EmployeeManagerTests
+    {
+        [TestMethod()]
+        public void AddEmployeeTest()
+        {
+            //arrange the test data with mocking.
+            var employee = new EmployeeEntities.Employee
+            {
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "vinuth" + DateTime.Now.Millisecond + ""
+            };
+            var mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            mockEmployeeRepository
+                .Setup(x => x.AddEmployee(employee))
+                .Returns(1);
+
+            //arrange 
+            var employeeManager = new EmployeeManager(mockEmployeeRepository.Object);
+            var result = employeeManager.AddEmployee(employee);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result, 1);
+
+        }
+
+        [TestMethod()]
+        public void DeleteEmployeeTest()
+        {
+            //arrange
+            var mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            mockEmployeeRepository
+                .Setup(x => x.DeleteEmployee(1))
+                .Returns(true);
+
+            //arrange 
+            var employeeManager = new EmployeeManager(mockEmployeeRepository.Object);
+            var result = employeeManager.DeleteEmployee(1);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result, false);
+        }
+
+        [TestMethod]
+        public void DeleteEmployeeErrorTest()
+        {
+            //arrange
+            var mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            mockEmployeeRepository
+                .Setup(x => x.DeleteEmployee(0))
+                .Returns(false);
+
+            //arrange 
+            var employeeManager = new EmployeeManager(mockEmployeeRepository.Object);
+            var result = employeeManager.DeleteEmployee(0);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result, false);
+        }
+
+        [TestMethod()]
+        public void GetEmployeeTest()
+        {
+            //arrange
+            var employee = new EmployeeEntities.Employee
+            {
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "vinuth" + DateTime.Now.Millisecond + "",
+                Id = 1
+            };
+            var mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            mockEmployeeRepository
+                .Setup(x => x.GetEmployee(1))
+                .Returns(employee);
+
+            //arrange 
+            var employeeManager = new EmployeeManager(mockEmployeeRepository.Object);
+            var result = employeeManager.GetEmployee(1);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Id, employee.Id);
+        }       
+
+
+        [TestMethod()]
+        public void GetEmployeesTest()
+        {
+            var employees = new List<EmployeeEntities.Employee>
+            {
+                new EmployeeEntities.Employee{
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "VinuthTest1",
+                Id = 1
+
+                },
+                new EmployeeEntities.Employee{
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "VinuthTest2",
+                Id = 2
+                }
+            };
+            var mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            mockEmployeeRepository
+                .Setup(x => x.GetEmployees())
+                .Returns(employees);
+
+            //act: calling method with mocked object
+            var employeeManager = new EmployeeManager(mockEmployeeRepository.Object);
+            var result = employeeManager.GetEmployees();
+
+            //assert: verifying the result
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod()]
+        public void CheckLoginTest()
+        {
+
+            var employee = new EmployeeEntities.Employee
+            {
+                DateOfBirth = "27-04-1995",
+                Email = "vinuthcv@abc.com",
+                FullName = "vinuth vish",
+                Gender = "Male",
+                Password = "test123",
+                Username = "vinuth" + DateTime.Now.Millisecond + "",
+                Id = 1
+            };
+
+            var mockEmployeeRepository = new Mock<IEmployeeRepository>();
+            mockEmployeeRepository
+                .Setup(x => x.GetEmployee(employee.Username))
+                .Returns(employee);
+
+            //act: calling method with mocked object
+            var employeeManager = new EmployeeManager(mockEmployeeRepository.Object);
+            var result = employeeManager.CheckLogin(employee.Username,employee.Password);
+
+            //assert: verifying the result
+            Assert.IsNotNull(result);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
